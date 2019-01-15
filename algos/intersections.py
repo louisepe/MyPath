7e96a963041a510ab06807e6f3fc10f77296214d
mariaaliciaLich <mariaaliciab8@gmail.com>
2019-01-15 17:50:03 +0100

    Update intersections.py

--- algos/intersections.py
+++ algos/intersections.py
@@ -1,4 +1,3 @@
-
 import osmnx as ox, networkx as nx
 from IPython.display import IFrame
 import random
@@ -13,11 +12,11 @@ location_point_id=5204021270
 location_point_coord = (33.2989041, -111.833341)
 
 # Define de distance of exploration
-G = ox.graph_from_point(location_point_coord, distance=200, simplify=False)
+G = ox.graph_from_point(location_point_coord, distance=300, simplify=False)
 # This is only to visualise the point in a graph t 
 
 
-######################### Simplify the graph (have only the intersections) THIS IS ONLY A TEST ####################################
+######################### Simplify the graph (have only the intersections) ####################################
 
 G2 = G.copy()
 G2 = ox.simplify_graph(G2)
@@ -28,15 +27,11 @@ fig, ax = ox.plot_graph(G2, node_color='b', node_zorder=3)
 nodes = ox.graph_to_gdfs(G2, edges=False)
 nodes[['x', 'y']]
 
-#######SHORTEST PATH TEST#############
+#######SHORTEST PATH#############
 
 origin_node = list(G2.nodes())[0]
 destination_node = list(G2.nodes())[9]
-
-#shortest path calculation
 route = nx.shortest_path(G2, origin_node, destination_node)
-
-#this is to see the path in a html file that you have to create in data/route.html
 route_map = ox.plot_route_folium(G2, route)
 filepath = 'data/route.html'
 route_map.save(filepath)
@@ -44,22 +39,23 @@ IFrame(filepath, width=600, height=500)
 
 
 d=[route]
-
-############CALCULATION OF A CIRCULAR PATH#########
+############CALCUL DU PATH#########
 distance=10000
-#the divisions is only to see how many different scan and shortest path we'll do
 divisions=int(distance/200)
 parcour=0
 length=0
 route=[]
-#testlocation_id=5204021270
-#testLocation_coord = (33.2989041, -111.833341)
-#origintest={'y':33.2989041 , 'x': -111.833341 , 'osmid':5204021270}
-
+print(divisions)
+testlocation_id=5204021270
+testLocation_coord = (33.2989041, -111.833341)
+origintest={'y':33.2989041 , 'x': -111.833341 , 'osmid':5204021270}
 origintest2=list(G2.nodes())[0]
+lx=[]
+ly=[]
 for x in range(1,divisions+1):
-	
-	#Simplification of the nodes, we use only intersections
+	xcoord=G2.node[origintest2]['x']
+	ycoord=G2.node[origintest2]['y']
+	ori_coord=(ycoord, xcoord)
 	G = ox.graph_from_point(testLocation_coord, distance=300, simplify=False)
 	G2 = G.copy()
 	G2 = ox.simplify_graph(G2)
@@ -67,14 +63,12 @@ for x in range(1,divisions+1):
 
 	nodesTest = ox.graph_to_gdfs(G2, edges=False)
 	nodesTest[['x', 'y']]
-	#use of a random destination node in the aerea
 	destination_node = list(G2.nodes())[random.randint(1,len(G2.nodes())-1)]
 	lat2=G2.node[destination_node]['y']
 	lon2=G2.node[destination_node]['x']
-	#Calculation of the distance between the 2 nodes
 	lat1=G2.node[origintest2]['y']
 	lon1=G2.node[origintest2]['x']
-	
+
 	sin1 = math.sin(math.radians(lat1))
 	sin2 = math.sin(math.radians(lat2))
 	cos1 = math.cos(math.radians(lat1))
@@ -82,45 +76,54 @@ for x in range(1,divisions+1):
 	cos3 = math.cos(math.radians(lon2-lon1))
 
 	distancenoeuds = 6367445*math.acos((sin1*sin2) + (cos1*cos2*cos3))
-	#route of the shortest path
 	route2 = nx.shortest_path(G2, origintest2, destination_node)
+	for j in range(0,len(route2)):
+		ly.append(G2.node[route2[j]]['y'])
+		lx.append(G2.node[route2[j]]['x'])
+		pass
+	print(route2)
+
+	#fig, ax = ox.plot_graph_route(G2, route2)
 	route= route + route2
-	#compilation of all the nodes collected 
-	route.append(route2)
-	#calculation of the total distance
 	parcour=parcour+ distancenoeuds
 	origintest2=destination_node
 
 
 	pass
+#Return to starting node
+if destination_node!=origin_node:
+	destination_node = origin_node
+	lat2=G2.node[destination_node]['y']
+	lon2=G2.node[destination_node]['x']
+	lat1=G2.node[origintest2]['y']
+	lon1=G2.node[origintest2]['x']
 
-#find the route to the origin point
+	sin1 = math.sin(math.radians(lat1))
+	sin2 = math.sin(math.radians(lat2))
+	cos1 = math.cos(math.radians(lat1))
+	cos2 = math.cos(math.radians(lat2))
+	cos3 = math.cos(math.radians(lon2-lon1))
 
-destination_node = origin_node
-lat2=G2.node[destination_node]['y']
-lon2=G2.node[destination_node]['x']
-lat1=G2.node[origintest2]['y']
-lon1=G2.node[origintest2]['x']
-
-sin1 = math.sin(math.radians(lat1))
-sin2 = math.sin(math.radians(lat2))
-cos1 = math.cos(math.radians(lat1))
-cos2 = math.cos(math.radians(lat2))
-cos3 = math.cos(math.radians(lon2-lon1))
-
-distancenoeuds = 6367445*math.acos((sin1*sin2) + (cos1*cos2*cos3))
-route2 = nx.shortest_path(G2, origintest2, destination_node)
-route.append(route2)
-parcour=parcour+ distancenoeuds
-
+	distancenoeuds = 6367445*math.acos((sin1*sin2) + (cos1*cos2*cos3))
+	route2 = nx.shortest_path(G2, origintest2, destination_node)
+	for j in range(0,len(route2)):
+		ly.append(G2.node[route2[j]]['y'])
+		lx.append(G2.node[route2[j]]['x'])
+		pass
+	print(route2)
+	#fig, ax = ox.plot_graph_route(G2, route2)
+	route=route+route2
+	parcour=parcour+ distancenoeuds
+	pass
 
 
-#################PRINT THE RESULTS#############################
+
+
+#PRINT THE RESULTS
 print(nodes)
 print(list(G2.nodes()))
 
 ids=[]
-#print nodes of the last graph
 for x in range(0,len(list(G2.nodes()))):
 	id=list(G2.nodes())[x]
 	lat=G2.node[id]
@@ -128,13 +131,25 @@ for x in range(0,len(list(G2.nodes()))):
 	print(lat)
 	pass
 print(ids)
+############################## PRINT FOR TESTS ##############################
 print('ROUTE DU PLUS COURT CHEMIN')
 print(route)
 print(d)
 print('DISTANCE')
 print(parcour)
 print(origin_node)
+print(type(ori_coord))
+print(type(testLocation_coord))
 
+############################## CORDINATES LIST GENERATION##############################
+routelat=[]
+for n in range(0,len(lx)):
+	routelat.append('0')
 
+for n in range(0,len(lx)):
+	routelat[n]="("+str(ly[n])+','+str(lx[n])+")"
+	pass
+############################## PRINT in a .txt ##############################
+print(routelat)
 with open('data.txt', 'w') as outfile:  
-    json.dump(ids, outfile)
+    json.dump(routelat, outfile)
